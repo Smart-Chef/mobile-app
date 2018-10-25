b064f7a1ca98c02ae3d1f18ef3b67fcd0b19758d
Blair Bryant <blair@Gradys-MacBook-Pro.local>
2018-10-25 14:10:35 -0500

    Initial Commit

---
+++ Smart Chef*/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smart Chef*
+//
+//  Created by Blair Bryant on 10/25/18.
+//  Copyright © 2018 Blair Bryant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
